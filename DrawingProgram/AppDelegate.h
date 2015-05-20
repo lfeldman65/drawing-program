40176d5075a60ccdd8c8408f7db20d5e0ac058c0
Larry Feldman <larryfeldman@Larrys-MacBook-Pro.local>
2015-05-20 11:17:29 -0500

    Initial Commit

---
+++ DrawingProgram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DrawingProgram
+//
+//  Created by Larry Feldman on 5/20/15.
+//  Copyright (c) 2015 Larry Feldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DrawingProgram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DrawingProgram
+//
+//  Created by Larry Feldman on 5/20/15.
+//  Copyright (c) 2015 Larry Feldman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
