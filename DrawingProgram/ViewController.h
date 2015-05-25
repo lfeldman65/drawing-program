349ad1672d3754553191432ebf1c49d2a53ad388
Larry Feldman <larryfeldman@Larrys-MacBook-Pro.local>
2015-05-25 12:44:17 -0500

    Basic functionality completed for various colors and eraser.

--- DrawingProgram/ViewController.h
+++ DrawingProgram/ViewController.h
@@ -8,18 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController {
-    
-    CGPoint lastPoint;
-    CGPoint moveBackTo;
-    CGPoint currentPoint;
-    CGPoint location;
-    NSDate *lastClick;
-    BOOL mouseSwiped;
-    UIImageView *drawImage;
-    UIImageView *frontImage;
-    
-}
+@interface ViewController : UIViewController
+
+@property (nonatomic) CGPoint lastPoint;
+@property (nonatomic) CGPoint currentPoint;
+@property (nonatomic) CGPoint location;
+@property (nonatomic) NSDate *lastClick;
+@property (nonatomic) BOOL mouseSwiped;
+@property (nonatomic) UIImageView *drawImage;
+@property (nonatomic) CGFloat red;
+@property (nonatomic) CGFloat green;
+@property (nonatomic) CGFloat blue;
+@property (nonatomic) CGFloat brushWidth;
+@property (nonatomic) CGFloat opacity;
+@property (nonatomic) CGFloat screenWidth;
+@property (nonatomic) CGFloat screenHeight;
+
+
 
 
 @end
--- DrawingProgram/ViewController.m
+++ DrawingProgram/ViewController.m
@@ -10,35 +10,55 @@
 
 @interface ViewController ()
 
+
+- (IBAction)colorPressed:(UIButton *)sender;
+
+
+
 @end
 
 @implementation ViewController
 
+@synthesize lastPoint;
+@synthesize currentPoint;
+@synthesize location;
+@synthesize lastClick;
+@synthesize mouseSwiped;
+@synthesize drawImage;
+@synthesize red, green, blue;
+@synthesize brushWidth;
+@synthesize opacity;
+@synthesize screenHeight;
+@synthesize screenWidth;
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    screenWidth = [UIScreen mainScreen].bounds.size.width;
+    screenHeight = [UIScreen mainScreen].bounds.size.height;
 
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     drawImage.image = [defaults objectForKey:@"drawImageKey"];
     drawImage = [[UIImageView alloc] initWithImage:nil];
     drawImage.frame = self.view.frame;
+    
+    red = 0.0/255.0;
+    green = 0.0/255.0;
+    blue = 0.0/255.0;
+    brushWidth = 10.0;
+    opacity = 1.0;
+    
     [self.view addSubview:drawImage];
 }
 
 
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     
+    mouseSwiped = NO;
     UITouch *touch = [[event allTouches] anyObject];
-    
-    if ([touch tapCount] == 2) {
-        drawImage.image = nil;
-    }
-    
     location = [touch locationInView:touch.view];
-    lastClick = [NSDate date];
-    
     lastPoint = [touch locationInView:self.view];
-    lastPoint.y -= 0;
-    
     [super touchesBegan:touches withEvent:event];
     
 }
@@ -49,17 +69,17 @@
     UITouch *touch = [touches anyObject];
     currentPoint = [touch locationInView:self.view];
     
-    UIGraphicsBeginImageContext(CGSizeMake(320, 568));
-    [drawImage.image drawInRect:CGRectMake(0, 0, 320, 568)];
+    UIGraphicsBeginImageContext(CGSizeMake(screenWidth, screenHeight));
+    [drawImage.image drawInRect:CGRectMake(0, 0, screenWidth, screenHeight)];
     CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound);
-    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 5.0);
-    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), 0, 1, 0, 1);
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brushWidth);
+    CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), red, green, blue, 1);
     CGContextBeginPath(UIGraphicsGetCurrentContext());
     CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
     CGContextAddLineToPoint(UIGraphicsGetCurrentContext(), currentPoint.x, currentPoint.y);
     CGContextStrokePath(UIGraphicsGetCurrentContext());
     
-    [drawImage setFrame:CGRectMake(0, 0, 320, 568)];
+    [drawImage setFrame:CGRectMake(0, 0, screenWidth, screenHeight)];
     drawImage.image = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
     lastPoint = currentPoint;
@@ -69,9 +89,74 @@
 }
 
 
+- (IBAction)colorPressed:(UIButton *)sender {
+    
+    switch(sender.tag)
+    {
+        case 0:                         // black
+            red = 0.0/255.0;
+            green = 0.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 1:
+            red = 255.0/255.0;          // red
+            green = 0.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 2:
+            red = 0.0/255.0;            // green
+            green = 255.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 3:                         // blue
+            red = 0.0/255.0;
+            green = 0.0/255.0;
+            blue = 255.0/255.0;
+            break;
+        case 4:                         // yellow
+            red = 255.0/255.0;
+            green = 255.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 5:                         // orange
+            red = 255.0/255.0;
+            green = 127.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 6:                         // purple
+            red = 127.0/255.0;
+            green = 0.0/255.0;
+            blue = 127.0/255.0;
+            break;
+        case 7:
+            red = 160.0/255.0;
+            green = 82.0/255.0;
+            blue = 45.0/255.0;
+            break;
+        case 8:
+            red = 255.0/255.0;
+            green = 102.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 9:
+            red = 255.0/255.0;
+            green = 255.0/255.0;
+            blue = 0.0/255.0;
+            break;
+        case 10:                        // eraser
+            red = 255.0/255.0;
+            green = 255.0/255.0;
+            blue = 255.0/255.0;
+            break;
+    }
+}
+
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
